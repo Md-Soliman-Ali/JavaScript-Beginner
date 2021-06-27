88771025ad910d3b4ad169544cc31181b9084e3b
Md. Soliman Ali <65532591+Md-Soliman-Ali@users.noreply.github.com>
2021-06-27 20:08:49 +0600

    Add files via upload

---
+++ Arithmetic Operators/js/script.js
@@ -0,0 +1,25 @@
+/* var a = 5;
+var b = 7;
+
+a++;
+++b;
+
+document.write(a);
+document.write("\n");
+document.write(b); */
+
+var a = 5;
+var b = 7;
+
+var c, d;
+
+c = a++;
+d = ++b;
+
+document.write(c); 
+document.write("\n");
+document.write(a);
+document.write("\n");
+document.write(d);
+document.write("\n");
+document.write(b);
