2d7e2d00be905ff3bc602c5af50205b37fd512c8
Md. Soliman Ali <65532591+Md-Soliman-Ali@users.noreply.github.com>
2021-06-28 12:56:12 +0600

    Add files via upload

---
+++ Arrays/js/script.js
@@ -0,0 +1,62 @@
+/* countries = ["Bangladesh" , "UK" , "China"];
+document.write(countries); */
+
+/* countries = ["Bangladesh" , "UK" , "China"];
+document.write(countries[1]); */
+
+/* countries = ["Bangladesh" , "UK" , "China"];
+document.write(countries.length); */
+
+/* countries = ["Bangladesh" , "UK" , "China"];
+document.write(countries.pop()); */
+
+/* countries = ["Bangladesh" , "UK" , "China"];
+document.write(countries.push("Poland"));
+document.write(" ",countries); */
+
+/* countries = ["Bangladesh" , "UK" , "China"];
+document.write(countries.shift());
+document.write(" ",countries); */
+
+/* countries = ["Bangladesh" , "UK" , "China"];
+document.write(countries.unshift("India"));
+document.write(" ",countries); */
+
+/* var x = "Bangladesh";
+var y = x.split("");
+console.log(y); */
+
+/* var x = "Bangladesh";
+var y = x.split("a");
+console.log(y); */
+
+
+/* var x = "Bangladesh Is A Country";
+var y = x.split(" ");
+console.log(y); */
+
+/* var x = "Bangladesh Is A Country";
+var y = x.split(" ");
+var z = y.join("/");
+console.log(z); */
+
+/* var x = ["Bangladesh", "India", "China"];
+var y = ["Japan"];
+var z = x.concat(y);
+console.log(z); */
+
+/* var x = ["Bangladesh", "India", "China"];
+var y = x.sort();
+console.log(y); */
+
+/* var x = ["Bangladesh", "India", "China"];
+var y = x.reverse();
+console.log(y); */
+
+/* var x = [2, 4, 6, ["A" , "B"], 8, 10];
+console.log(x);
+console.log(x[3][1]); */
+
+/* var x = [2, 4, 6, ["A" , "B"], {name: "omar"}, 8, 10];
+console.log(x[4]["name"]);
+console.log(x[4].name); */
